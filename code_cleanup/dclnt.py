e4be5850535d3abd24105f66044fc7b6e0dba2df
Ivan Nikiforov <nikiforr@gmail.com>
2018-01-08 19:44:56 +0300

    Vars renamed, fixed file list in get_trees()

--- code_cleanup/dclnt.py
+++ code_cleanup/dclnt.py
@@ -17,22 +17,22 @@ def is_verb(word):
     return pos_info[0][1] == 'VB'
 
 
-Path = ''
+path = os.curdir
 
 
 def get_trees(_path, with_filenames=False, with_file_content=False):
-    filenames = []
+    py_files = []
     trees = []
-    path = Path
-    for dirname, dirs, files in os.walk(path, topdown=True):
-        for file in files:
+    # path = Path
+    for dirpath, dirnames, filenames in os.walk(path, topdown=True):
+        for file in filenames:
             if file.endswith('.py'):
-                filenames.append(os.path.join(dirname, file))
-                if len(filenames) == 100:
+                py_files.append(os.path.join(dirpath, file))
+                if len(py_files) == 100:
                     break
-    print('total %s files' % len(filenames))
-    for filename in filenames:
-        with open(filename, 'r', encoding='utf-8') as attempt_handler:
+    print('total %s files' % len(py_files))
+    for filename in py_files:
+        with open(filename, encoding='utf-8') as attempt_handler:
             main_file_content = attempt_handler.read()
         try:
             tree = ast.parse(main_file_content)
@@ -58,7 +58,7 @@ def get_verbs_from_function_name(function_name):
     return [word for word in function_name.split('_') if is_verb(word)]
 
 
-def get_all_words_in_path(path):
+def get_all_words_in_path(_path):
     trees = [t for t in get_trees(path) if t]
     function_names = [f for f in flat([get_all_names(t) for t in trees])
                       if not (f.startswith('__') and f.endswith('__'))]
@@ -69,10 +69,10 @@ def get_all_words_in_path(path):
                  for function_name in function_names])
 
 
-def get_top_verbs_in_path(path, top_size=10):
-    global Path
-    Path = path
-    trees = [t for t in get_trees(None) if t]
+def get_top_verbs_in_path(_path, top_size=10):
+    # global Path
+    # Path = path
+    trees = [t for t in get_trees(path) if t]
     fncs = [f for f in flat([[node.name.lower() for node in ast.walk(t) if isinstance(
         node, ast.FunctionDef)] for t in trees]) if not (f.startswith('__') and f.endswith('__'))]
     print('functions extracted')
