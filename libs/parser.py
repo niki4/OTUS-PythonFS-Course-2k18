d1d4b793d5c3d43fe6d5f18439ca4f47e3ff3a84
Ivan Nikiforov <nikiforr@gmail.com>
2018-01-31 00:52:12 +0300

    trees list generation moved inside 'with' context manager to ensure attempt_handler closed correctly in any case

--- libs/parser.py
+++ libs/parser.py
@@ -17,19 +17,17 @@ def get_trees(path, with_filenames=False, with_file_content=False):
     py_files = get_files(path)
     for filename in py_files:
         with open(filename, encoding='utf-8') as attempt_handler:
-            main_file_content = attempt_handler.read()
+            try:
+                tree = ast.parse(attempt_handler.read())
+            except SyntaxError as exc_info:
+                raise SyntaxError('Syntax Error %s' % exc_info)
 
-        try:
-            tree = ast.parse(main_file_content)
-        except SyntaxError as exc_info:
-            raise SyntaxError('Syntax Error %s' % exc_info)
-
-        if with_filenames:
-            if with_file_content:
-                trees.append((filename, main_file_content, tree))
+            if with_filenames:
+                if with_file_content:
+                    trees.append((filename, attempt_handler.read(), tree))
+                else:
+                    trees.append((filename, tree))
             else:
-                trees.append((filename, tree))
-        else:
-            trees.append(tree)
+                trees.append(tree)
     print('%s trees generated' % len(trees))
     return trees
